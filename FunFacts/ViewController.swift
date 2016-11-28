51a8979921cf64ee6a7aa795856dfe71147e3cb8
Jeremiah Jacquet <Jeremiah@whisper.sh>
2016-11-28 00:21:27 -0800

    updating with random color model

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -9,25 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var FunFactLabel: UILabel!
+    @IBOutlet weak var FunFactButton: UIButton!
     let factProvider = FactProvider()
+    let colorProvider = BackgroundColorProvider()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         FunFactLabel.text = factProvider.randomFact()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func showFact() {
         FunFactLabel.text = factProvider.randomFact()
-        
+        let randomColor = colorProvider.randomColor()
+        view.backgroundColor = randomColor
+        FunFactButton.tintColor = randomColor
     }
-
 }
 
